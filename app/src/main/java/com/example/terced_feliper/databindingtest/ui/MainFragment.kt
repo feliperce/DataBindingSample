a23699c97310259967a2d8e3eb9c3d0352e0f47a
Felipe Rodrigues <felipe.celestino@ftd.com.br>
2018-06-29 17:51:38 -0300

    implemented viewmodel

--- app/src/main/java/com/example/terced_feliper/databindingtest/ui/MainFragment.kt
+++ app/src/main/java/com/example/terced_feliper/databindingtest/ui/MainFragment.kt
@@ -18,10 +18,11 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val binding: MainFragmentBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
                 inflater, R.layout.main_fragment, container, false)
 
         val view = binding.root
@@ -32,7 +33,8 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewmodel = viewModel
+
     }
 
 }
--- app/src/main/java/com/example/terced_feliper/databindingtest/ui/MainViewModel.kt
+++ app/src/main/java/com/example/terced_feliper/databindingtest/ui/MainViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.terced_feliper.databindingtest.ui
 
 import androidx.databinding.ObservableField
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.terced_feliper.databindingtest.model.User
 
 class MainViewModel : ViewModel() {
     var name = ObservableField("fieldd")
+
+    fun changeName(user: User) {
+        this.name.set(user.name)
+    }
 }
